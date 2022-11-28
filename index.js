9b372309a10a31bf7065fa8368a25a99f64bc239
karen okasaki <oks.karen@gmail.com>
2022-11-28 11:22:28 -0300

    primeira rota

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ const app = express();
 //configurar o servidor para aceitar enviar e receber arquivos em JSON
 app.use(express.json());
 
+//CRIAÇÃO DAS ROTAS
+app.get("/enap", (req, res) => {
+  // req -> request -> REQUISIÇÃO QUE VEM DO CLIENTE
+  // res -> response -> RESPOSTA PARA O CLIENTE
+
+  const bemVindo = "Bem vindo ao servidor da ENAP turma 92 - Ironhack";
+
+  //retorna uma resposta com status de 200 e um json .....
+  return res.status(200).json({ msg: bemVindo, turma: "92 web dev" });
+});
+
 // o servidor subindo pro ar.
-app.listen(8080, () => {
-  console.log("App up and running on port http://localhost:8080");
+app.listen(process.env.PORT, () => {
+  console.log(
+    `App up and running on port http://localhost:${process.env.PORT}`
+  );
 });
